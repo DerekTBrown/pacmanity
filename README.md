a7c46f20e334cccbbe949eaf4b7be1c275acfae7
Alex Chernokun <alexchernokun@gmail.com>
2016-10-25 14:27:26 +0300

    Update pacmanity_aur.sh
    Update pacmanity.sh
    
    Update pacmanity_aur.sh
    
    Update pacmanity_aur.sh
    
    Update pacmanity.sh
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Pacmanity
-Packages which installed via pacman and yaourt in your Arch Linux
+List of packages installed via pacman and yaourt on your Arch machine automatically saved to GitHub.
+> *all ideas and improvments are always welcome!*
+
+
+## Installation
+
+**1) Downloading and installing:**
+
+- using yaourt [Aur](https://aur.archlinux.org/packages/pacmanity/)
+```
+$ yaourt pacmanity
+```
+
+- using makepkg
+```
+$ wget https://github.com/alexchernokun/pacmanity/tarball/master -O - | tar xz
+```
+
+navigate to the downloaded directory (alexchernokun-pacmanity-xxxxxx):
+
+```
+$ makepkg
+$ sudo pacman -U 
+```
+
+**2) Setup stage:**
+On the installation stage you will be prompted to fill in GitHub credentials two times;
+1) first time it is needed to create gist for pacman-installed packages on GItHub;
+2) second time gist with AUR-installed packages should be set up, that is why the login details should be filled in once again
+
+
+## Usage
+
+Every time you install or remove a package using [official Arch repository](https://www.archlinux.org/packages/) or unofficial [AUR](https://aur.archlinux.org/) all the changes will be saved privately to your GitHub account.
+You have nothing to do, just install the pacmanity, and that's all.
+Two hooks will be created and run everytime the pacman command is used.
+
+**Screenshots:**
+Navigate to your Gist GitHub account here and you will see:
+- list of packages installed with 'pacman -S' command:
+![pacman](http://image.prntscr.com/image/cf15521e7b794acdb37b2a8bc5e4455c.png)
+- list of packages installed from AUR with yaourt:
+![AUR](http://image.prntscr.com/image/d5bb89e7020d4b18a236d94adf3eb32f.png)
+- every installation will be automatically added to the gist:
+![after_installation](http://image.prntscr.com/image/65eeb152529e4b1dbab78de777074679.png)
+- every removal also will be mentioned on the list:
+![after_removal](http://image.prntscr.com/image/3d945ff4d17e460a99dd1382cfb8689d.png)
+
+<details> 
+  <summary>*P.S.: : *And now for something completely different:</summary>
+    ![bachmanity](https://pbs.twimg.com/media/Cjegi2dVAAEOU2n.jpg) 
+</details>
--- src/pacmanity_aur.sh
+++ src/pacmanity_aur.sh
@@ -7,40 +7,41 @@ pacmanity_aur(){
     PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 
     # Load Config
-    [[ -r '/etc/pacmanity' ]] && source $pkgdir/etc/pacmanity
+    [[ -r '/etc/pacmanity_aur' ]] && source $pkgdir/etc/pacmanity_aur
 
     # Determine if Fresh Install is Needed
     if [ -z "$GIST_ID" ]; then
-      echo -e "\nError: Gist Backup not Setup."
-      echo -e "\nRun yaourt -S pacmanity and follow instructions."
+      echo -e "\nInfo: Gist backup for apps installed from AUR not setup."
+      echo -e "\nplease fill in your GitHub credentials one more time to log into GitHub Gist."
     else
       pacmanity_aur_update;
     fi
 }
 
 pacmanity_aur_install(){
-  echo -e "\nGist Backup is a alpm hook which saves an updated"
-  echo -e "version of your package list privately to your GitHub Account"
-  echo -e "with every package installation.\n";
+  echo -e "\nApps installed via 'yaourt' command will be"
+  echo -e "saved to the second package list privately to your GitHub Account.";
 
-  echo -e "\nStep 1: Login to Gist";
+  echo -e "\nStep 1: Fill in GitHub credentials one more time"
+  echo -e "to create the second list with yaourt-installed apps";
   gist --login;
   mkdir -p $pkgdir/root;
   cp ~/.gist $pkgdir/root/.gist;
 
-  echo -e "\nStep 2: Creating Backup";
+  echo -e "\nStep 2: Creating list with yaourt-installed apps";
   GIST_URL=$(pacman -Qqem | gist -p -f arch_yaourt -d "Arch Packages installed from AUR")
 
   echo "GIST_ID=$GIST_URL" | sed 's/https:\/\/gist.github.com\///g' >> $pkgdir/etc/pacmanity_aur;
 
   echo -e "\nYour package list is safely backed up, and will be updated"
-  echo -e "automatically every time you install a package using a package manager,"
-  echo -e "You can view your backup at the link below:\n";
+  echo -e "automatically every time you install/remove a package using the yaourt."
+  echo -e "You can view your backup lists at https://gist.github.com/user"
+  echo -e "or directly at the link below:\n";
   echo "$GIST_URL";
 }
 
 pacmanity_aur_update(){
-  echo -e "\nUpdating Package List Backup...";
+  echo -e "\nUpdating package list backup on GitHub...";
   if pacman -Qqem | gist -u "$GIST_ID"; then
     echo -e "Success!\n";
   else
